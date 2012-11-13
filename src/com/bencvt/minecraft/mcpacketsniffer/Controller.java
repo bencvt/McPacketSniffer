709eaf09bb986a9d4f3c049c8b3f8fe16f9a27c1
Ben Cartwright <bencvt@gmail.com>
2012-11-13 17:00:04 -0500

    post 3.1 release

--- src/com/bencvt/minecraft/mcpacketsniffer/Controller.java
+++ src/com/bencvt/minecraft/mcpacketsniffer/Controller.java
@@ -17,7 +17,7 @@ import com.bencvt.minecraft.mcpacketsniffer.Util.FileLogger;
  */
 public class Controller implements ClientPacketEventListener {
     public static final String NAME = "McPacketSniffer";
-    public static final String VERSION = "3.0-SNAPSHOT [1.4.2]";
+    public static final String VERSION = "3.1-SNAPSHOT [1.4.4]";
     public static final long OPTIONS_CHECK_RELOAD_INTERVAL = 20000;
     public static final long DUMP_STATS_INTERVAL = 30000;
 
